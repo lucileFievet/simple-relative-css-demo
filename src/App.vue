d936ebd93ea44df3efa1b0a145507c49b3e1e915
Lucile Fievet <lucile.fievet@awacloud.com>
2018-06-08 15:06:27 +0200

    working relative scss inside style tag

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<!-- works fine -->
+<style lang="scss" src="./App.scss">
+@import './assets/vars/colors.scss'
+</style>
